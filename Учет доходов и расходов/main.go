e39692138dc7bc5a9d544791b59b249285cb1b11
Petrov Evgeny <PetrovE2@ya.ru>
2025-04-22 04:30:28 +0300

    add main.go
    Для приложения "Учёт доходов и расходов"

---
+++ Учет доходов и расходов/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Slice для хранения всех транзакций
+	transactions := []float64{}
+	for {
+		// Сканируем транзакцию от пользователя
+		transaction := scanTransaction()
+		if transaction == 0 {
+			// Если введено 0, выходим из цикла
+			break
+		}
+		// Добавляем транзакцию в список
+		transactions = append(transactions, transaction)
+	}
+	// Рассчитываем общий баланс
+	balance := calculateBalance(transactions)
+	fmt.Printf("Ваш баланс: %.2f", balance)
+}
+
+// scanTransaction: Функция для ввода транзакции от пользователя
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Введите транзакцию (0 для выхода): ")
+	fmt.Scan(&transaction)
+	return transaction
+}
+
+// calculateBalance: Функция для расчета общего баланса
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	// Суммируем все транзакции
+	for _, value := range transactions {
+		balance += value
+	}
+	return balance
+}
